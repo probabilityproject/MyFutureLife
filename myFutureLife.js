da8633e6a9142cd067ccf0bcd46b56f3452d4180
probabilityproject <79235919+probabilityproject@users.noreply.github.com>
2023-05-15 09:47:49 -0700

    Update myFutureLife.js

--- myFutureLife.js
+++ myFutureLife.js
@@ -1,33 +1,53 @@
+let computerScienceBuilding = document.getElementById("computerProgrammerPicture");
+
 var computerScienceLife = [
-    "Computer Science Picture",
-    "Computer Science Text"
+    computerScienceBuilding,
+    "I graduated from Caltech with a masters in Computer Science! After graduating, I immediately began looking for a programming",
+    "job. Luckily, since I live in Pasadena, there are a lot of job opportunities available to me. I live in a small studio apartment by",
+    "myself, and I plan on moving north eventually in order to buy a small house."
     ];
+
 var engineerLife = [
     "Engineer Picture",
-    "Engineer Text"
+    "I graduated from Caltech with a masters in Engineering!"
     ];
+
 var revealComputerScience = false;
 var revealEngineer = false;
+// Sets up the global variables
 
-let computerScienceButton = document.getElementById('situation-one')
+let computerScienceButton = document.getElementById("situation-one");
+computerScienceButton.addEventListener("click", firstButton);
 
-computerScienceButton.addEventListener("click" evt => {
-
-    let firstButton = document.getElementById('computer-programmer')
-    firstButton.innerHTML = '<img src="images/blankprogrammer.jpg">';
-    revealComputerScience = true
-    revealEngineer = false
-
-});
+function firstButton() {
+    computerScienceBuilding.innerHTML = '<img src="downloads/images/computerScienceBuilding.jpg">'
+    document.getElementById("computerProgrammer").innerHTML = computerScienceLife;
+    document.getElementById("engineer").innerHTML = "";
+    revealComputerScience = true;
+    revealEngineer = false;
+    
+    //if revealComputerScience = true {
+      //  while (i < 10);
+        //computerScienceLife = i;
+        //i++;
+    //};
+};
 // Reveals the life as a computer programmer portion
 
-let engineerLifeButton = document.getElementById('situation-two')
-    
-engineerLifeButton.addEventListener("click" evt => {
+let engineerLifeButton = document.getElementById("situation-two");
+engineerLifeButton.addEventListener("click", secondButton);
 
-    let secondButton = document.getElementById('engineer')
-    secondButton.innerHTML = '<img src="images/blankengineer.jpg">'
-    revealComputerScience = false
-    revealEngineer = true
-});
+function secondButton() {
+    document.getElementById("engineer").innerHTML = engineerLife;
+    document.getElementById("computerProgrammer").innerHTML = "";
+    revealComputerScience = false;
+    revealEngineer = true;
+    
+    //if revealEngineer = true {
+    //    while (i < 10);
+      //  engineerLife += i;
+        //i++;
+  //  };
+};
 // Reveals the life as an engineer portion
+    
